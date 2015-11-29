7f09680bae4a267573791a5de4000556ac664fbb
chhavi1991gupta <chhavi.gupta@sjsu.edu>
2015-11-29 00:58:28 -0800

    new additions

--- routes/analysis.js
+++ routes/analysis.js
@@ -1,161 +1,154 @@
-
+/**
+ * Created by shubhiyede on 17/11/2015.
+ */
 var mysql = require('./mysql');
 var tweetStats = require('./twitterSearch');
 
 exports.getStats = function (req, res) {
 
-    var compFirst = req.param("comp1").toLowerCase().trim();
-    var compSecond = req.param("comp2").toLowerCase().trim();
-    var getStats = "select * from mytable1 where Company_Name='" + compFirst + "' OR Company_Name='" + compSecond + "'";
-    var polarity = [];
-    tweetStats.getTweets(compFirst, compSecond, function (err, result, twits) {
+    var compFirst = req.param("comp1");//.toLowerCase().trim();
+    var criteria = "";//"stock equity asset liability revenue EBITDA profit loss cash up down";
+    //var compSecond = req.param("comp2").toLowerCase().trim();
+    var getStats = "select * from mytable1 where Company_Name='" + compFirst + "'";
+    console.log(getStats);
+    var polarity = [0,0,0];
+    var analysisResult;
+    tweetStats.getTweets(compFirst, criteria, function (err, result, twits) {
         if (err) {
 
             throw err;
         }
 
-        polarity = result;
+        analysisResult = result;
         var tweets = twits;
-        console.log("POLARITY: ..." + polarity);
+        console.log("score="+analysisResult.score);
 
         mysql.fetchData(getStats, function (err, rows) {
-            if (rows.length < 6) {
+            console.log("rows="+rows.length);
+            if (rows.length < 3) {
 
                 res.render('index', {error: "Error"});
                 console.log(err);
 
             }
             else {
-                console.log("pol..." + polarity);
+                //console.log("pol..." + polarity);
+                
+                var year2012="2012";
+                var year2013="2013";
+                var year2014="2014";
+                var i;
                 var comp1 = [];
                 var comp2 = [];
+                var current_Share_Value=1;
+                
                 for (var i = 0; i < rows.length; i++) {
+                  
                     if (rows[i].Company_Name.toUpperCase() === compFirst.toUpperCase()) {
                         comp1.push(rows[i]);
+                        console.log(rows[i].Share_Values);
+                        if(rows[i].Year.toUpperCase() === year2014.toUpperCase()){
+                          current_Share_Value=rows[i].Share_Values;
+                        }
                     }
                     else if (rows[i].Company_Name.toUpperCase() === compSecond.toUpperCase()) {
                         comp2.push(rows[i]);
                     }
-
-                    //rows[i].Company_Name.toUpperCase() === compFirst.toUpperCase()
-
                 }
+                var avg = (comp1[0].Share_Values+comp1[1].Share_Values+comp1[2].Share_Values)/3;
+                var increase_by = avg-current_Share_Value;
+                var predict_share_value = parseInt((increase_by*100)/current_Share_Value);
+                var selected;
 
-                var EV1 = [];
-                var EV2 = [];
-                //var asset_ratio1 = [];
-                //var asset_ratio2 = [];
-                var operating_MarginRatio1 = [];
-                var operating_MarginRatio2 = [];
-                var debt_equity1 = [];
-                var debt_equity2 = [];
-                var year = [];
-
-
-                for (var i = 0; i < 3; i++) {
-
-                    var counter = 0;
-                    if (comp1.sector == comp2.sector) {
-                        console.log("in sector");
-                        counter = counter + 1;
-                        console.log(counter);
-                    }
-
-                    console.log("at Total_Assets :" + comp2[i]);
-                    console.log("at Total_Liabilities :" + comp2[i].Total_Liabilities);
-
-                    EV1.push((comp1[i].Total_Assets - comp1[i].Total_Liabilities));
-
-                    EV2.push((comp2[i].Total_Assets - comp2[i].Total_Liabilities));
-                    console.log("at EV 2 :" + EV2[i]);
-
-                    debt_equity1.push((comp1[i].Total_Liabilities / EV1[i]));
-                    debt_equity2.push((comp2[i].Total_Liabilities / EV2[i]));
-                    //asset_ratio1.push((comp1[i].Total_Assets / comp1[i].Total_Current_Assets));
-                    //asset_ratio2.push((comp2[i].Total_Assets / comp2[i].Total_Current_Assets));
-                    operating_MarginRatio1.push((comp1[i].EBITDA / comp1[i].total_revenue));
-                    operating_MarginRatio2.push((comp2[i].EBITDA / comp2[i].total_revenue));
-
-                    console.log("value Total_Current_Assets 1.." + comp1[i].Total_Current_Assets);
-                    console.log("value Total_Current_Assets 2.." + comp2[i].Total_Current_Assets);
-                    console.log("Company 1: " + JSON.stringify(comp1));
-                    console.log("Company 2: " + JSON.stringify(comp1))
-
-                    if (EV1[i] > 2 * EV2[i]) {
-                        counter = counter + 1;
-                        console.log("in EV.." + counter);
-                    }
-                    if (debt_equity1[i] > 2 * debt_equity2[i]) {
-                        counter = counter + 1;
-                        console.log("in debt_equity.." + counter);
-
-                    }
-                    //if (asset_ratio1[i] > 3 * asset_ratio2[i]) {
-                    //    counter = counter + 1;
-                    //    console.log("in asset_ratio.." + counter);
-                    //}
-                    if (operating_MarginRatio1[i] > operating_MarginRatio2[i]) {
-                        counter = counter + 1;
-                        console.log("in operating_MarginRatio1.." + counter);
-
-
-                    }
-                    if (comp1[i].Total_Current_Assets > (3 * comp2[i].Total_Current_Assets)) {
-                        console.log("in asset");
-                        counter = counter + 1;
-                        console.log(counter);
-                    }
-                    if (comp1[i].Total_Liabilities > (2 * comp2[i].Total_Liabilities)) {
-                        console.log("in gross");
-                        console.log("in total");
-                        counter = counter + 1;
-                        console.log(counter);
-                    }
-                    if (comp1[i].gross_profit > 3 * comp2[i].gross_profit) {
-                        counter = counter + 1;
-                        console.log(counter);
-                    }
-                    if (comp1[i].Share_Values > comp2[i].Share_Values) {
-                        console.log("in share");
-                        counter = counter + 1;
-                        console.log(counter);
-                    }
-                    if (comp1[i].Number_of_Employees > comp2[i].Number_of_Employees) {
-                        console.log("in full");
-                        counter = counter + 1;
-                        console.log(counter);
-                    }
-                    if (comp1[i].Gross_Margin > 3 * comp2[i].Gross_Margin) {
-                        console.log("in gross margin");
-                        counter = counter + 1;
-                        console.log(counter);
-                    }
-                    if (comp1[i].Cash_and_Cash_Equivalents > 2 * comp2[i].Cash_and_Cash_Equivalents) {
-                        console.log("in cash");
-                        counter = counter + 1;
-                        console.log(counter);
-                    }
-                    year.push(Math.round((counter / 11) * 100));
-
-
+                console.log("CLUSTERING RESULT");
+                console.log("Cluster 1 for words: ["+analysisResult.feature1Words+"] is of density="+analysisResult.clusters[0].length+" and sentimentScore="+analysisResult.feature1Sentiment);
+                console.log("Cluster 2 for words: ["+analysisResult.feature2Words+"] is of density="+analysisResult.clusters[1].length+" and sentimentScore="+analysisResult.feature2Sentiment);
+                if(analysisResult.clusters[1].length > analysisResult.clusters[0].length){
+                  selected=2;
                 }
-                console.log("y1: " + year[0] + " y2: " + year[1] + " y3: " + year[2]);
-                var finalPercent = parseInt(((year[0] + year[1] + year[2]) / 3));
-                console.log("final percent: " + finalPercent);
+                else{
+                  selected=1;
+                }
+                if(selected==1){
+                  var difference = analysisResult.feature1Sentiment - analysisResult.feature2Sentiment;
+                  //analysisResult.score -> analysisResult.feature1Sentiment
+                  //100                  -> ?
+                  var rate = (100*analysisResult.feature1Sentiment)/analysisResult.score;
+                  if(difference>0){
+                    //Stock rate will increase
+                    console.log("Stock will increase by "+rate);
+                    
+                  }
+                  else{
+                    //Stock rate will decrease
+                    console.log("Stock will decrease by "+rate);
+                    
+                  }
+                }
+                else if(selected==2){
+                  var difference = analysisResult.feature2Sentiment - analysisResult.feature1Sentiment;
+                  //analysisResult.score -> analysisResult.feature2Sentiment
+                  //100                  -> ?
+                  var rate = (100*analysisResult.feature2Sentiment)/analysisResult.score;
+                  if(difference>0){
+                    //Stock rate will increase
+                    console.log("Stock will increase by "+rate);
+                    
+                  }
+                  else{
+                    //Stock rate will decrease
+                    console.log("Stock will decrease by "+rate);
+                    
+                  }
+                }
+                predict_share_value=avg*(100+rate)/100;
+                console.log("Year 2012 Stock rate = "+comp1[0].Share_Values);
+                console.log("Year 2013 Stock rate = "+comp1[1].Share_Values);
+                console.log("Year 2014 Stock rate = "+comp1[2].Share_Values);
+                console.log("AVG = "+avg);
+                console.log("The new value of stock for year 2015 is "+predict_share_value);
+                var shareValues=[];
+                shareValues.push(comp1[0].Share_Values);
+                shareValues.push(comp1[1].Share_Values);
+                shareValues.push(comp1[2].Share_Values);
+                shareValues.push(predict_share_value);
+                
+                //var clustersCopy = analysisResult.clusters;
+                //var cluster1 = clustersCopy[0];
+                //for(var i=0)
 
-                //console.log(JSON.stringify(rows));
+                /*
+                var result = {
+        score:          score,
+        vsm:            vsm,
+        feature1Words:  feature1Words,
+        feature2Words:  feature2Words,
+        feature1Sentiment:  feature1Sentiment,
+        feature2Sentiment:  feature2Sentiment,
+        VS:             VS,
+        US:             US,
+        clusters:       clusters
+    };
+                */
+                
                 res.render('viewStats', {
                     data: rows,
                     comp1: comp1,
                     comp2: comp2,
                     polarity: polarity,
                     tweets: tweets,
-                    finalPercent: finalPercent
+                    shareValues: shareValues,
+                    feature1Words:  analysisResult.feature1Words,
+                    feature2Words:  analysisResult.feature2Words,
+                    feature1Sentiment:  analysisResult.feature1Sentiment,
+                    feature2Sentiment:  analysisResult.feature2Sentiment,
+                    clusters:       analysisResult.clusters
                 });
             }
             // render or error
 
         });
+        
     });
 
 
